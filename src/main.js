cfac920d82d5a22fa37ecff874c9606c0b1c2afe
Tanyiqu <1953649096@qq.com>
2021-02-20 11:03:28 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import Vue from 'vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import MyAxiosHttp from '@/plugin/http.js';
+Vue.use(MyAxiosHttp);
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
