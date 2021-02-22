9a9c467a0878b2af04325f77ba4e091b80366307
Tanyiqu <1953649096@qq.com>
2021-02-22 18:19:45 +0800

    实现课程学时页面布局 修改学时等

---
+++ src/components/WeekPeriod.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="week-period">
+    <!-- 周 -->
+    <div class="week-item hover-shadow">
+      <i class="el-icon-s-opportunity" style="color: #d0d5db"></i>
+      <span>{{ weekData.week_name }}</span>
+    </div>
+
+    <!-- 学时 -->
+    <div
+      class="period-item hover-shadow"
+      v-for="(period, index) in weekData.periods"
+      :key="index"
+    >
+      <span class="period-name">
+        {{ period.period_name }}
+      </span>
+
+      <span v-show="period.period_type === 1" class="tag">理论课</span>
+      <span v-show="period.period_type === 2" class="tag">实验课</span>
+
+      <code v-show="period.period_status === 1" class="status">开放中</code>
+      <code v-show="period.period_status === 2" class="status">已结束</code>
+      
+      <el-button
+        class="edit"
+        type="primary"
+        size="mini"
+        round
+        @click="handleEdit(period)"
+        >编辑</el-button
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      value: true
+    }
+  },
+
+  props: ['weekData'],
+
+  methods: {
+    // 点击编辑，传递给父组件调用
+    handleEdit(period) {
+      this.$emit('editPeriod', period);
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.week-period {
+  margin: 10px 30px;
+  //   background: pink;
+
+  // 鼠标悬浮阴影
+  .hover-shadow {
+    height: 40px;
+    line-height: 40px;
+    font-size: 16px;
+    padding-left: 14px;
+    cursor: pointer;
+
+    &:hover {
+      background: #f7fafc;
+      border-radius: 4px;
+    }
+  }
+
+  .week-item {
+    font-weight: bold;
+    color: #000;
+    i {
+      width: 16px;
+      height: 16px;
+    }
+
+    span {
+      margin-left: 14px;
+    }
+  }
+
+  .period-item {
+    position: relative;
+    margin: 6px 0;
+    margin-left: 30px;
+    color: #181e33;
+
+    .period-name {
+      width: 200px;
+      max-width: 200px;
+    }
+
+    .tag {
+      position: absolute;
+      height: 30px;
+      line-height: 30px;
+      top: 5px;
+      left: 450px;
+      padding: 0 5px;
+      border-radius: 4px;
+      background: #ecf5ff;
+      margin-left: 16px;
+    }
+
+    .status {
+      position: absolute;
+      height: 30px;
+      line-height: 30px;
+      top: 5px;
+      left: 535px;
+      padding: 0 5px;
+      border-radius: 4px;
+      background: #ecf5ff;
+      margin-left: 16px;
+    }
+
+    .edit {
+      top: 5px;
+      left: 610px;
+      position: absolute;
+      margin-left: 30px;
+      display: none;
+    }
+
+    &:hover {
+      .edit {
+        display: block;
+      }
+    }
+  }
+}
+</style>
--- src/views/Course/menu/Period.vue
+++ src/views/Course/menu/Period.vue
@@ -5,36 +5,164 @@
       v-loading="loading"
       element-loading-text="正在加载中，请稍等..."
     >
-      <ul>
-        <ul>
-          第一周
-          <li>001</li>
-          <li>002</li>
-        </ul>
-
-        <ul>
-          第二周
-          <li>003</li>
-        </ul>
-
-        <ul>
-          第三周
-          <li>004</li>
-          <li>005</li>
-        </ul>
-      </ul>
+      <div class="weekperiod-list">
+        <WeekPeriod
+          v-for="(week, index) in weeks"
+          :week-data="week"
+          :key="index"
+          @editPeriod="editPeriod"
+        />
+      </div>
     </div>
+
+    <!-- 对话框 -->
+    <!-- 编辑period的对话框 -->
+    <el-dialog title="编辑学时" :visible.sync="editDialogVisible" width="45%">
+      <el-form :model="editPeriodForm" label-position="left">
+        <el-form-item label="名 称" label-width="50px">
+          <el-input v-model="editPeriodForm.periodName"></el-input>
+        </el-form-item>
+
+        <el-form-item label="类 型" label-width="50px">
+          <el-select v-model="editPeriodForm.periodType" placeholder="性别">
+            <el-option
+              v-for="item in typeOpts"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="类 型" label-width="50px">
+          <el-select v-model="editPeriodForm.periodStatus" placeholder="性别">
+            <el-option
+              v-for="item in statusOpts"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false"> 取 消 </el-button>
+        <el-button type="primary" @click="onEditClick"> 确 定 </el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import WeekPeriod from '@/components/WeekPeriod.vue';
 export default {
 
   data() {
     return {
       courseId: 0,
+      loading: true,
 
-      loading: true
+      weeks: [
+        {
+          week_id: 1,
+          week_name: '第01周',
+          periods: [
+            {
+              period_content: "内容",
+              period_id: 1,
+              period_name: "第01学时 - 概要",
+              period_status: 1,
+              period_type: 1,
+              week_id: 1
+            },
+            {
+              period_content: "内容",
+              period_id: 2,
+              period_name: "第02学时 - html基础",
+              period_status: 1,
+              period_type: 1,
+              week_id: 1,
+              week_id: 1
+            }
+          ]
+        }
+      ],
+
+      editDialogVisible: false,
+      editPeriodForm: {
+        periodId: 0,
+        periodName: '',
+        periodType: 1,
+        periodStatus: 1
+      },
+      typeOpts: [{
+        value: 1,
+        label: '理论课'
+      }, {
+        value: 2,
+        label: '实验课'
+      }],
+      statusOpts: [{
+        value: 1,
+        label: '开放中'
+      }, {
+        value: 2,
+        label: '已结束'
+      }]
+    }
+  },
+
+  components: {
+    WeekPeriod
+  },
+
+  methods: {
+    // 刷新数据
+    async refreshWeekPeriod() {
+      this.loading = true;
+
+      let [data, err] = await this.$awaitWrap(this.$get('weekperiod/select', {
+        id: this.courseId
+      }));
+      if (err) {
+        this.$message.warning(err);
+        return;
+      }
+      console.log(data);
+      this.weeks = data.data;
+
+      // 关闭loading
+      this.loading = false;
+    },
+
+    // 监听子组件点击编辑
+    editPeriod(period) {
+      this.editPeriodForm.periodId = period.period_id;
+      this.editPeriodForm.periodName = period.period_name;
+      this.editPeriodForm.periodType = period.period_type;
+      this.editPeriodForm.periodStatus = period.period_status;
+      this.editDialogVisible = true;
+    },
+
+
+    // 点击确定编辑
+    async onEditClick() {
+      let [data, err] = await this.$awaitWrap(this.$post('weekperiod/updateperiod', {
+        id: this.editPeriodForm.periodId,
+        name: this.editPeriodForm.periodName,
+        type: this.editPeriodForm.periodType,
+        status: this.editPeriodForm.periodStatus
+      }));
+      if (err) {
+        this.$message.warning(err);
+        return;
+      }
+      this.$message.success(data.msg);
+      this.editDialogVisible = false;
+      this.refreshWeekPeriod();
     }
   },
 
@@ -48,19 +176,7 @@ export default {
   // 加载数据
   async beforeMount() {
     this.courseId = this.$route.query.courseid;
-    // let [data, err] = await this.$awaitWrap(this.$get('weekperiod/select', {
-    //   id: this.courseId
-    // }));
-    // if (err) {
-    //   this.$message.warning(err);
-    //   return;
-    // }
-    // console.log(data);
-
-    setTimeout(() => {
-      this.loading = false;
-    }, 500);
-
+    this.refreshWeekPeriod();
   }
 }
 </script>
@@ -83,6 +199,11 @@ export default {
     border-radius: 16px;
     background: #fff;
     box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    overflow: auto;
+
+    .weekperiod-list {
+      margin-top: 26px;
+    }
   }
 }
 </style>
