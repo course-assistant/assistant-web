a9ea8243bceb2b8575c7d549daaea6c9bbca8d12
Tanyiqu <1953649096@qq.com>
2021-02-23 15:30:36 +0800

    班级管理

--- src/views/Course/menu/ClassManage.vue
+++ src/views/Course/menu/ClassManage.vue
@@ -1,11 +1,60 @@
 <template>
   <div class="container">
-    <div class="round-div">班级</div>
+    <div class="round-div">
+      <!-- 操作按钮 -->
+      <div class="options">
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          round
+          @click="handleAddClass"
+          >添加班级
+        </el-button>
+      </div>
+
+      <!-- 班级列表 -->
+      <div class="class-list">
+        <ClassItem
+          v-for="(cls, index) in classes"
+          :key="index"
+          :class-data="cls"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ClassItem from '@/components/ClassItem.vue';
+
 export default {
+
+  data() {
+    return {
+      classes: [
+        {
+          classId: 1,
+          className: '0814171',
+          classStuNum: 10
+        },
+        {
+          classId: 2,
+          className: '0814172',
+          classStuNum: 12
+        }
+      ]
+    }
+  },
+
+  components: { ClassItem },
+
+  methods: {
+
+    // 点击添加班级
+    handleAddClass() {
+      console.log('添加班级');
+    }
+  },
 
   beforeCreate() {
     // 检有没有携带参数
@@ -16,7 +65,7 @@ export default {
 
 
   beforeMount() {
-    
+
   }
 }
 </script>
@@ -39,6 +88,14 @@ export default {
     border-radius: 16px;
     background: #fff;
     box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+
+    .options {
+      padding: 20px 0 0 40px;
+    }
+
+    .class-list {
+      margin-top: 20px;
+    }
   }
 }
 </style>
