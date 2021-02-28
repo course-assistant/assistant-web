d4e0cc20f3a40447de5cf3f347a6e9237dea5239
Tanyiqu <1953649096@qq.com>
2021-02-28 12:38:17 +0800

    课堂讨论页面

--- src/views/Course/menu/Discuss.vue
+++ src/views/Course/menu/Discuss.vue
@@ -6,54 +6,108 @@
       element-loading-text="正在加载中，请稍等..."
     >
       <!-- 讨论 -->
-      <div class="discussions">
-        <!-- 左 -- 周 -->
-        <div class="discusstion-week">
-          <el-menu
-            :default-active="weekPeriods[0].periods[0].period_id.toString()"
-            class="el-menu-vertical-demo"
-            @select="selectPeriod"
-            unique-opened
+      <!-- 左 -- 周 -->
+      <div class="discusstion-week">
+        <el-menu
+          :default-active="weekPeriods[0].periods[0].period_id.toString()"
+          class="el-menu-vertical-demo"
+          @select="selectPeriod"
+          unique-opened
+        >
+          <el-submenu
+            v-for="(week, i) in weekPeriods"
+            :key="i"
+            :index="week.week_id.toString()"
           >
-            <el-submenu
-              v-for="(week, i) in weekPeriods"
-              :key="i"
-              :index="week.week_id.toString()"
-            >
-              <template slot="title">
-                <i class="el-icon-s-order"></i>
-                <span>{{ week.week_name }}</span>
-              </template>
+            <template slot="title">
+              <i class="el-icon-s-order"></i>
+              <span>{{ week.week_name }}</span>
+            </template>
 
-              <el-menu-item-group>
-                <template slot="title">学时</template>
-                <el-menu-item
-                  v-for="(period, ind) in week.periods"
-                  :key="ind"
-                  :index="period.period_id.toString()"
-                >
-                  {{ period.period_name }}
-                </el-menu-item>
-              </el-menu-item-group>
-            </el-submenu>
-          </el-menu>
-        </div>
+            <el-menu-item-group>
+              <template slot="title">学时</template>
+              <el-menu-item
+                v-for="(period, ind) in week.periods"
+                :key="ind"
+                :index="period.period_id.toString()"
+              >
+                {{ period.period_name }}
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </div>
 
-        <!-- 右 -- 讨论 -->
-        <div class="discusstion-list">
-          
+      <!-- 右 -- 讨论 -->
+      <div class="discusstion-list">
+        <el-button
+          type="primary"
+          round
+          @click="addDiscussionDialogVisible = true"
+          >添加讨论</el-button
+        >
+        <p style="margin: 20px 8px">全部话题</p>
+        <el-divider />
+
+        <div class="discussions">
+          <DiscussionItem
+            v-for="(discussion, index) in discussions"
+            :key="index"
+          />
         </div>
       </div>
     </div>
+
+    <!-- 对话框 -->
+    <el-dialog
+      title="添加讨论"
+      :visible.sync="addDiscussionDialogVisible"
+      width="50%"
+    >
+      <el-form :model="addDiscussionForm" label-position="left">
+        <el-form-item>
+          <el-input
+            v-model="addDiscussionForm.title"
+            placeholder="请输入标题"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-input
+            type="textarea"
+            :rows="7"
+            placeholder="请输入内容"
+            v-model="addDiscussionForm.content"
+            clearable
+          >
+          </el-input>
+        </el-form-item>
+      </el-form>
+
+      <p style="color: #a8a8b3; font-size: 14px">
+        发表该话题即表示您已阅读并接受<a href="" target="_blank">《用户协议》</a
+        >, 请勿发布色情，反动等违法内容
+      </p>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addDiscussionDialogVisible = false" clearable>
+          取 消
+        </el-button>
+        <el-button type="primary" @click="onAddDiscussion"> 确 定 </el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import DiscussionItem from '@/components/DiscussionItem.vue';
 
 export default {
 
   data() {
     return {
+      loading: true,
+
       courseId: 0,
 
       weekPeriods: [
@@ -88,14 +142,43 @@ export default {
             }
           ]
         }
-      ]
+      ],
+
+      discussions: [
+        {
+          discussion_id: 1,
+          discussion_title: '标题',                                                                 
+          discussion_content: '内容内天  ad啊ad',
+          discussion_date: '2021-1-1 12:34'
+        },
+        {
+          discussion_id: 2,
+          discussion_title: '标题22',
+          discussion_content: '内22222222',
+          discussion_date: '2021-1-1 23:45'
+        }
+      ],
+
+      addDiscussionDialogVisible: false,
+      addDiscussionForm: {
+        title: '',
+        content: ''
+      }
     }
   },
+
+  components: { DiscussionItem },
 
   methods: {
     // 点击某一学时
     selectPeriod(index) {
       console.log(index);
+    },
+
+
+    // 点击确定添加讨论
+    onAddDiscussion() {
+      console.log(this.addDiscussionForm);
     }
   },
 
@@ -147,22 +230,22 @@ export default {
     border-radius: 16px;
     background: #fff;
     box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    display: flex;
 
-    .discussions {
-      width: 100%;
-      height: 100%;
-      display: flex;
+    .discusstion-week {
+      flex: 1;
+      margin: 16px 0;
+      overflow: auto;
+    }
 
-      .discusstion-week {
-        flex: 1;
-        // background: khaki;
-        margin: 16px 0;
-        overflow: auto;
-      }
+    .discusstion-list {
+      flex: 5;
+      margin: 20px;
+      overflow: auto;
 
-      .discusstion-list {
-        flex: 5;
-        background: violet;
+      p {
+        color: #a8a8b3;
+        font-size: 12px;
       }
     }
   }
